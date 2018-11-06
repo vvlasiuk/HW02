bf7c99fc46647517964810625b5c9a33b161e05e
vvlasiuk <vlsv@ukr.net>
2018-11-06 14:12:19 +0200

    Step 8

---
+++ MyFirstApp/app/src/main/java/net/ukr/vlsv/myfirstapp/SecondActivity.kt
@@ -0,0 +1,29 @@
+package net.ukr.vlsv.myfirstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.textview_random
+import kotlinx.android.synthetic.main.activity_second.textview_label
+import java.util.Random
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        showRandomNumber()
+    }
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    fun showRandomNumber() {
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+        val random = Random()
+        var randomInt = 0
+        if (count > 0) {
+            randomInt = random.nextInt(count + 1)
+        }
+        textview_random.text = Integer.toString(randomInt)
+        textview_label.text = getString(R.string.random_heading, count)
+    }}
